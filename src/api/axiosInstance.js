43a3eb7f840c8358e1b9249598ed94526c43d937
Michael-starz <sirmichaelchibuike@gmail.com>
2026-04-07 12:48:05 +0100

    bug fixes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 // src/api/axiosInstance.js
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
